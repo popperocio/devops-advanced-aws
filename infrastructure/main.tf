d630083d1c3a4d469ed3f085d8bea2866703df0b
rocio poppe <rocio.poppe@ioet.com>
2024-08-05 17:07:15 -0300

    add terraform config to manage s3, s3 permissions and s3 url

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -40,3 +40,49 @@ output "lambda_function_url" {
   value = aws_lambda_function_url.lambda_function_url.function_url
 }
 
+
+#****************************** FRONTEND ********************************************
+
+resource "aws_s3_bucket" "frontend_bucket" {
+  bucket = "frontend-bucket-pokemon-app"
+  tags = {
+    Name = "Frontend Bucket"
+  }
+}
+
+resource "aws_s3_object" "object" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+  key    = "index.html"
+  source = "/ui/build/index.html"
+  acl    = "public-read"
+  depends_on = [aws_s3_bucket.frontend_bucket]
+}
+
+resource "aws_s3_bucket_policy" "frontend_bucket_policy" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = "*"
+        Action = "s3:GetObject"
+        Resource = "${aws_s3_bucket.frontend_bucket.arn}/*"
+      }
+    ]
+  })
+}
+
+resource "aws_s3_bucket_public_access_block" "frontend_bucket_public_access" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+output "s3_bucket_url" {
+  value = "http://localhost:4566/${aws_s3_bucket.frontend_bucket.bucket}/index.html"
+}
